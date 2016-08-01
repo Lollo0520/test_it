1d9b9b8bdd508505b26acc18549748cfeef35f22
Jet Wang <929705164@qq.com>
2016-08-02 06:18:24 +0800

    "Upload from home pc 8/2"

---
+++ app/src/main/java/com/example/jetwang/testitout/SimpleDrawingView.java
@@ -0,0 +1,72 @@
+package com.example.jetwang.testitout;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by Jet Wang on 2016/8/2.
+ */
+public class SimpleDrawingView extends View {
+
+    private final int paintColor = Color.BLACK;
+    private Paint drawPaint;
+    private Path path = new Path();
+    private Bitmap mCacheBitmap = null;
+    private Canvas mCacheCanvas = null;
+
+    public SimpleDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    void init(Context context){
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(touchX, touchY);
+                break;
+            default:
+                return false;
+        }
+        postInvalidate();
+        return true;
+    }
+}
