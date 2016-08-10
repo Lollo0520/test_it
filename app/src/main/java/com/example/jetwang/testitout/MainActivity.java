3bd5cac7a5beed1fb2863a301cf2060d79ca6c6e
jet wang <mulang.wang@nanosic.com>
2016-08-10 17:37:25 +0800

    upload from work pc 8/10

--- app/src/main/java/com/example/jetwang/testitout/MainActivity.java
+++ app/src/main/java/com/example/jetwang/testitout/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.jetwang.testitout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager mWifiManager;
+    private List<String> mWifiNameList = new ArrayList<>();
+    private ArrayAdapter<String> mAdapter;
+
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                List<ScanResult> mScanResults = mWifiManager.getScanResults();
+                for (ScanResult result : mScanResults){
+                    mWifiNameList.add(result.SSID);
+                    mAdapter.notifyDataSetChanged();
+                }
+                // add your logic here
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_02);
+        ListView wifiList = (ListView) findViewById(R.id.wifi_list);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mWifiNameList);
+        wifiList.setAdapter(mAdapter);
+        wifiList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String wifiName = mWifiNameList.get(position);
+                final EditText et = (EditText) getLayoutInflater().inflate(R.layout.input_pass_et, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(wifiName)
+                        .setView(et)
+                        .setPositiveButton("connect", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String password = et.getText().toString();
+                                connectToWifi(wifiName, password);
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
+
+            }
+        });
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        registerReceiver(mWifiScanReceiver,
+                new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mWifiManager.startScan();
+    }
+
+    public void onWifiBtnClick(View view) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$DataUsageSummaryActivity"));
+        startActivity(intent);
+    }
+
+    private void connectToWifi(String ssid, String password){
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        // setup a wifi configuration
+        WifiConfiguration wc = new WifiConfiguration();
+        wc.SSID = "\""+ ssid+ "\"";
+        wc.preSharedKey = "\"" + password + "\"";
+        wc.status = WifiConfiguration.Status.ENABLED;
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+// connect to and enable the connection
+        int netId = wifiManager.addNetwork(wc);
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
+//        wifiManager.setWifiEnabled(true);
     }
 }
